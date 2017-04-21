09d406909e77bbb3e959c35fa750d64769b41d08
mkearney@google.com <mkearney@google.com>
2017-04-20 21:23:01 -0700

    adding new table and updating products table

--- Bamazon.sql
+++ Bamazon.sql
@@ -3,11 +3,22 @@ CREATE DATABASE Bamazon_db;
 USE Bamazon_db;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(50) NULL,
   price DECIMAL(7,2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
-select * from products;
+Select * From products;
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs DECIMAL(7,2) NOT NULL,
+  total_sales DECIMAL(7,2) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+Select * From departments;
+
+ALTER TABLE products ADD COLUMN product_sales DECIMAL(7,2);
